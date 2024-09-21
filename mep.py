02922c3573e12705a015528b3495ffd9a8e30a0c
alkayesrifat <157808289+alkayesrifat@users.noreply.github.com>
2024-09-21 13:11:05 +0600

    Create mep.py

---
+++ mep.py
@@ -0,0 +1,267 @@
+userin = input("What you want to do (encode) or (decode) : ")
+if userin == "encode":
+    whatencode = input("What you want to encode : ")
+    spe = whatencode.split()
+    # print(len(spe))
+    print("Here is your Encoded Message")
+    #1
+    if len(spe) == 1:
+        spe[0]="ulj jokilk"+spe[0]+"lkjl"
+        spe.reverse()
+        re = ",".join(spe)
+        rest = re.replace(",","")
+        print(rest)
+        
+        #2
+    elif len(spe)==2:
+        spe[0]="hohdlk"+spe[0]+"lkli"
+        spe[1]="galk"+spe[1]+"lkri"
+        spe.reverse()
+        re = ",".join(spe)
+        rest = re.replace(",","")
+        print(rest)
+#3
+    elif len(spe)==3:
+        spe[0]="hohdlk"+spe[0]+"lkli"
+        spe[1]="galk"+spe[1]+"lkri"
+        spe[2]="galk"+spe[2]+"lkri"
+        spe.reverse()
+        rs = ",".join(spe)
+        rest = rs.replace(",","")
+        print(rest)
+    #4
+    elif len(spe)==4:
+        spe[0]="flumdriftlk"+spe[0]+"lkchilonkoljil"
+        spe[1]="gaskjilsihljosdflk"+spe[1]+"lkdskjfoasdfardsri"
+        spe[2]="gasdraeafatgalk"+spe[2]+"lksdrfasdfari"
+        spe[3]="rilijnsasravbflk"+spe[3]+"lkfsfafrazfrva"
+        spe.reverse()
+        rs = ",".join(spe)
+        rest = rs.replace(",","")
+        print(rest)
+        #5
+    elif len(spe)==5:
+        spe[0]="flumdriftlk"+spe[0]+"lkchilonkoljil"
+        spe[1]="gaskjilsihljosdflk"+spe[1]+"lkdskjfoasdfardsri"
+        spe[2]="gasdraeafatgalk"+spe[2]+"lksdrfasdfari"
+        spe[3]="rilijnsasravbflk"+spe[3]+"lkfsfafrazfrva"
+        spe[4]="gasdraeafatgalk"+spe[4]+"lksdrfasdfari"
+        spe.reverse()
+        rs = ",".join(spe)
+        rest = rs.replace(",","")
+        print(rest) 
+        #6
+    elif len(spe)==6:
+        spe[0]="fldsutlk"+spe[0]+"lkdsfjil"
+        spe[1]="ssrsrisdflk"+spe[1]+"lkskidsri"
+        spe[2]="sratgalk"+spe[2]+"lksdrfri"
+        spe[3]="rilibflk"+spe[3]+"lkfsfafrva"
+        spe[4]="gasaegalk"+spe[4]+"lksdrfari"
+        spe[5]="tornanirlk"+spe[5]+"lkgonalisa"
+        spe.reverse()
+        rs = ",".join(spe)
+        rest = rs.replace(",","")
+        print(rest) 
+        #7
+    elif len(spe)==7:
+        spe[0]="fldsutlk"+spe[0]+"lkdsfjil"
+        spe[1]="ssrsrisdflk"+spe[1]+"lkskidsri"
+        spe[2]="sratgalk"+spe[2]+"lksdrfri"
+        spe[3]="rilibflk"+spe[3]+"lkfsfafrva"
+        spe[4]="gasaegalk"+spe[4]+"lksdrfari"
+        spe[5]="tornanirlk"+spe[5]+"lkgonalisa"
+        spe[6]="sratgalk"+spe[6]+"lksdrfri"
+        spe.reverse()
+        rs = ",".join(spe)
+        rest = rs.replace(",","")
+        print(rest) 
+        #8
+    elif len(spe)==8:
+        spe[0]="fldsutlk"+spe[0]+"lkdsfjil"
+        spe[1]="ssrsrisdflk"+spe[1]+"lkskidsri"
+        spe[2]="sratgalk"+spe[2]+"lksdrfri"
+        spe[3]="rilibflk"+spe[3]+"lkfsfafrva"
+        spe[4]="gasaegalk"+spe[4]+"lksdrfari"
+        spe[5]="tornanirlk"+spe[5]+"lkgonalisa"
+        spe[6]="sratgalk"+spe[6]+"lksdrfri"
+        spe[7]="rilibflk"+spe[7]+"lkfsfafrva"
+        spe.reverse()
+        rs = ",".join(spe)
+        rest = rs.replace(",","")
+        print(rest) 
+        #9
+    elif len(spe)==9:
+        spe[0]="fldsutlk"+spe[0]+"lkdsfjil"
+        spe[1]="ssrsrisdflk"+spe[1]+"lkskidsri"
+        spe[2]="sratgalk"+spe[2]+"lksdrfri"
+        spe[3]="rilibflk"+spe[3]+"lkfsfafrva"
+        spe[4]="gasaegalk"+spe[4]+"lksdrfari"
+        spe[5]="tornanirlk"+spe[5]+"lkgonalisa"
+        spe[6]="sratgalk"+spe[6]+"lksdrfri"
+        spe[7]="rilibflk"+spe[7]+"lkfsfafrva"
+        spe[8]="fldsutlk"+spe[8]+"lkdsfjil"
+        spe.reverse()
+        rs = ",".join(spe)
+        rest = rs.replace(",","")
+        print(rest) 
+        #10
+    elif len(spe)==10:
+        spe[0]="fldsutlk"+spe[0]+"lkdsfjil"
+        spe[1]="ssrsrisdflk"+spe[1]+"lkskidsri"
+        spe[2]="sratgalk"+spe[2]+"lksdrfri"
+        spe[3]="rilibflk"+spe[3]+"lkfsfafrva"
+        spe[4]="gasaegalk"+spe[4]+"lksdrfari"
+        spe[5]="tornanirlk"+spe[5]+"lkgonalisa"
+        spe[6]="sratgalk"+spe[6]+"lksdrfri"
+        spe[7]="rilibflk"+spe[7]+"lkfsfafrva"
+        spe[8]="fldsutlk"+spe[8]+"lkdsfjil"
+        spe[9]="fldsutlk"+spe[9]+"lkdsfjil"
+        spe.reverse()
+        rs = ",".join(spe)
+        rest = rs.replace(",","")
+        print(rest) 
+#dlkjaldskjf;ladkjf;ldkas fj;lksdajf;lkksadj;lfkj sd;lkjf ;lksdjf;lkksdjf;lsdjf;lsdjf;lsdakjf;lsdkfj;lsdjf;l
+  
+elif userin == "decode":
+    Whatdecode = input("What you want to decode : ")
+    spt = Whatdecode.split("lk")
+    # print(spt)
+    # print(len(spt))
+    print("Here is your Decoded Message")
+#1
+    if len(spt)==3:
+         
+        
+        spt.reverse()
+        spt.remove("ulj joki")
+        spt.remove("jl")
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+        
+#2
+    elif len(spt)==5:
+        spt.reverse()
+        spt.remove("rihohd")
+        spt.remove("li")
+        spt.remove("ga")
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+#3
+    elif len(spt)==7:
+        spt.reverse()
+        spt.remove("rihohd")
+        spt.remove("li")
+        spt.remove("ga")
+        spt.remove("riga")
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+#4
+    elif len(spt)==9:
+        spt.reverse()
+        spt.remove("rilijnsasravbf")
+        spt.remove("fsfafrazfrvagasdraeafatga")
+        spt.remove("sdrfasdfarigaskjilsihljosdf")
+        spt.remove("dskjfoasdfardsriflumdrift")
+        spt.remove("chilonkoljil")
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+
+#5
+    elif len(spt)==11:
+        spt.reverse()
+        spt.remove("gasdraeafatga")
+        spt.remove("fsfafrazfrvagasdraeafatga")
+        spt.remove("sdrfasdfarigaskjilsihljosdf")
+        spt.remove("dskjfoasdfardsriflumdrift")
+        spt.remove("chilonkoljil")
+        spt.remove("sdrfasdfaririlijnsasravbf")
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+#6
+    elif len(spt)==13:
+        spt.reverse()
+        spt.remove("tornanir")
+        spt.remove("gonalisagasaega")
+        spt.remove("sdrfaririlibf")
+        spt.remove("fsfafrvasratga")
+        spt.remove("sdrfrissrsrisdf")
+        spt.remove("dsfjil")
+        spt.remove("skidsrifldsut")
+        
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+#7
+    elif len(spt)==15:
+        spt.reverse()
+        
+        spt.remove("gonalisagasaega")
+        spt.remove("sdrfaririlibf")
+        spt.remove("fsfafrvasratga")
+        spt.remove("sdrfrissrsrisdf")
+        spt.remove("dsfjil")
+        spt.remove("skidsrifldsut")
+        spt.remove("sratga")
+        spt.remove("sdrfritornanir")
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+#8
+    elif len(spt)==17:
+        spt.reverse()
+        spt.remove("rilibf")
+        spt.remove("fsfafrvasratga")
+        spt.remove("sdrfritornanir")
+        spt.remove("gonalisagasaega")
+        spt.remove("sdrfaririlibf")
+        spt.remove("fsfafrvasratga")
+        spt.remove("sdrfrissrsrisdf")
+        spt.remove("dsfjil")
+        spt.remove("skidsrifldsut")
+        
+        
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+#9
+    elif len(spt)==19:
+        spt.reverse()
+        spt.remove("fldsut")
+        spt.remove("dsfjilrilibf")
+        spt.remove("fsfafrvasratga")
+        spt.remove("sdrfritornanir")
+        spt.remove("gonalisagasaega")
+        spt.remove("sdrfaririlibf")
+        spt.remove("fsfafrvasratga")
+        spt.remove("sdrfrissrsrisdf")
+        spt.remove("skidsrifldsut")
+        spt.remove("dsfjil")
+        
+        
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
+#10
+    elif len(spt)==21:
+        spt.reverse()
+        spt.remove("fldsut")
+        spt.remove("dsfjilrilibf")
+        spt.remove("fsfafrvasratga")
+        spt.remove("sdrfritornanir")
+        spt.remove("gonalisagasaega")
+        spt.remove("sdrfaririlibf")
+        spt.remove("fsfafrvasratga")
+        spt.remove("sdrfrissrsrisdf")
+        spt.remove("skidsrifldsut")
+        spt.remove("dsfjil")
+        spt.remove("dsfjilfldsut")
+        
+        
+        st = ",".join(spt)
+        stt = st.replace(","," ")
+        print(stt)
